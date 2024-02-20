837508c20da21901bdec8974dcea1f52954083cd
Richa-12y <kricha000@gmail.com>
2024-02-20 21:59:05 +0530

    add the image and css about folder

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import React from "react";
+import styles from "./about.module.css";
 
 const AboutPage = () => {
   return (
     <div>
-      <Image src="/about.png" width={500} height={500} />
+      <div className={styles.imageContainer}>
+        <Image src="/about.png" width={500} height={500} fill />
+      </div>
     </div>
   );
 };
