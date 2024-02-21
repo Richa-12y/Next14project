cbb697696da1b2c79064c899b18a1a7b196ef862
Richa-12y <kricha000@gmail.com>
2024-02-21 21:25:21 +0530

    mainer change

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      // 'https://unsplash.com/photos/a-person-holding-an-umbrella-in-the-rain-3kBNzM9bDyA',
+      { protocol: "https", hostname: "www.pexels.com" },
+    ],
+  },
+};
 
 export default nextConfig;
