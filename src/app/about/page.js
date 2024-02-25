0cdcc425aa05124ab0b20a77d255cb95e48e7fcd
Richa-12y <kricha000@gmail.com>
2024-02-25 21:17:58 +0530

    working on contact page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -6,10 +6,34 @@ const AboutPage = () => {
   return (
     <div className={styles.conatiner}>
       <div className={styles.textContainer}>
-        <h2>About Agency</h2>
-        <h1>We create digital ideas that a bigger,bolder,brave and better</h1>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>
+          We create digital ideas that a bigger,bolder,brave and better
+        </h1>
+        <p className={styles.desc}>
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using content here, content here.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+        </div>
       </div>
-      <div className={styles.imageContainer}></div>
+      <div className={styles.imageContainer}>
+        <Image src="/about.png" alt="About Image" fill className={styles.img} />
+      </div>
     </div>
   );
 };
