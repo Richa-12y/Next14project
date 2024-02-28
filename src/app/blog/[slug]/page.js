9eb7dab0f0233fc6b838d8e33e935d51874e7a07
Richa-12y <kricha000@gmail.com>
2024-02-28 11:11:19 +0530

    working on single page blog

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,7 +1,19 @@
 import React from "react";
+import styles from "./singlePost.module.css";
+import Image from "next/image";
 
 const SinglePage = () => {
-  return <div>SinglePage</div>;
+  return (
+    <div className={styles.conatiner}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+      </div>
+      <div className={styles.textContainer}>hello</div>
+    </div>
+  );
 };
 
 export default SinglePage;
