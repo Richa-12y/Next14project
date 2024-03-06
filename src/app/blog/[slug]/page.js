70892e45457e0962ebd7786258f9a14ed5d8986e
Richa-12y <kricha000@gmail.com>
2024-03-06 22:10:29 +0530

    working on single post

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -9,9 +9,34 @@ const SinglePage = () => {
         <Image
           src="https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt=""
+          fill
+          className={styles.img}
         />
       </div>
-      <div className={styles.textContainer}>hello</div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image
+            src="https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            className={styles.avatar}
+            width={50}
+            height={50}
+          />
+        </div>
+        <div className={styles.detailText}>
+          <span className={styles.detailTitle}>Author</span>
+          <span className={styles.detailValue}>Date</span>
+        </div>
+        <div className={styles.detailText}>
+          <span className={styles.detailTitle}>Published</span>
+          <span className={styles.detailValue}>01.01.2024</span>
+        </div>
+        <div className={styles.content}>
+          survived not only five centuries, but also the leap into electronic
+          typesetting, remaining essentially unchanged. It was popularised in
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,7 @@ import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
-    <div className={styles.conatiner}>
+    <div className={styles.container}>
       <div className={styles.logo}>sheCode</div>
       <div className={styles.text}>
         sheCode creative thoughts agency © All right reserved.
